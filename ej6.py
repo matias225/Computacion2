b0c72710facbd69c797780feca908b2668a1eb60
Matias <mati.kbza22@gmail.com>
2020-04-25 21:36:38 -0300

    Empezado ej6

--- ej6.py
+++ ej6.py
@@ -1,5 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-def proc:
+import time
+from os import fork
+from signal import signal, SIGUSR1, getsignal
+
+def funcUSR1(s, frame):
     pass
 
+def main():
+    signal(SIGUSR1, funcUSR1)
+    if fork():
+        print("Soy el hijo")
+        
+if __name__ == '__main__':
+    main()
