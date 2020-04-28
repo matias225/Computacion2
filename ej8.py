511e527a0aa6c01a383c98baeb5a9100b0ae6795
Matias <mati.kbza22@gmail.com>
2020-04-28 18:59:54 -0300

    Agregado getopts a ejercicio 8

--- ej8.py
+++ ej8.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+import getopt
+import sys
+
+def getArg():
+    try:
+        (opts, arg) = getopt.getopt(sys.argv[1:], 'p:', ["process="])
+        return opts
+    except getopt.GetoptError as error:
+        print('Wrong Option: '+str(error)+'.')
+        exit()
+
+def getProcess(opts):
+    for (opt, arg) in opts:
+        if opt == '-p' or opt == "--process":
+            if arg.isdigit():
+                process = arg
+                print(process)
+            else:
+                print('Wrong Argument: "'+str(arg)+'" is not a digit.')
+
+getProcess(getArg())
