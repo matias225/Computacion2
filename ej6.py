5c5614dd64cfef2a2c5920f45604b684721e2ced
Matias <mati.kbza22@gmail.com>
2020-04-29 01:23:39 -0300

    Ejercicio 6 Terminado

--- ej6.py
+++ ej6.py
@@ -2,25 +2,48 @@
 
 import time
 from os import fork, wait, kill, getpid
-from signal import pause, signal, SIGUSR1, SIGTERM, getsignal
+from signal import pause, signal, SIGUSR1, SIGINT, SIGTERM, getsignal
 
 def funcUSR1(s, frame):
-    print("Señal USR1 recibida")
+    print("Señal USR1 de padre recibida")
+
+
+def funcINT(s, frame):
+    print("El usuario interrumpio el proceso padre. Cerrando...")
+    pid = getpid()
+    killSon(pid)
+    time.sleep(2)
     exit()
+
+
+def killSon(pid):
+    spid = pid+1
+    kill(spid, 9)
+
 
 def son():
     while True:
-        print("Hijo esperando")
+        print("Hijo esperando...")
         pause()
+
 
 def main():
     signal(SIGUSR1, funcUSR1)
+    signal(SIGINT, funcINT)
     pid = fork()
-    print(pid)
-    if pid:
-        print("Soy el hijo "+str(pid))
+    if pid == 0:
+        print("Iniciado proceso hijo "+str(getpid()))
         son()
-        #kill(pid, SIGTERM)
+    else:
+        print("Soy el padre, PID: "+str(getpid())) 
+        for a in range(10):
+            time.sleep(5)
+            kill(pid, SIGUSR1)
+        time.sleep(1)
+        print("Padre mata al hijo...")
+        kill(pid, 15)
+        print("Padre terminando...")
         
+
 if __name__ == '__main__':
     main()
