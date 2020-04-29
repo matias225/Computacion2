a67daf7a6c4b2290cc7b69f58bcdd968076b3c39
Matias <mati.kbza22@gmail.com>
2020-04-29 20:58:59 -0300

    Ejercicio 8 con un hijo

--- ej8.py
+++ ej8.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 
-import getopt
 import sys
+import time
+from getopt import getopt, GetoptError
+from os import fork, getpid, getppid, kill
+from signal import signal, SIGUSR2, SIGKILL, pause
+
+
+def funcUSR2(s, frame):
+    print("Soy el PID "+str(getpid())+", recibí la señal "+str(s)+" de mi padre PID "+str(getppid()))
+
 
 def getArg():
     try:
-        (opts, arg) = getopt.getopt(sys.argv[1:], 'p:', ["process="])
+        (opts, arg) = getopt(sys.argv[1:], 'p:', ["process="])
         return opts
-    except getopt.GetoptError as error:
+    except GetoptError as error:
         print('Wrong Option: '+str(error)+'.')
         exit()
+
+
+def son():
+    while True:
+        print("Hijo esperando...")
+        pause()
+
 
 def getProcess(opts):
     for (opt, arg) in opts:
@@ -21,8 +36,28 @@ def getProcess(opts):
                 print('Wrong Argument: "'+str(arg)+'" is not a digit.')
                 exit()
 
-def forking(numProcess):
-    print("Forkeando "+str(numProcess)+' procesos')
 
-numProcess = getProcess(getArg())
-forking(numProcess)
+def forking(numProcess):
+    signal(SIGUSR2, funcUSR2)
+    pid = fork()
+    if pid:
+        print("Padre PID: "+str(getpid()))
+        print("Creando proceso: "+str(pid))
+        time.sleep(3)
+        kill(pid, SIGUSR2)
+        time.sleep(3)
+        print("Padre matando hijo "+str(pid))
+        kill(pid, SIGKILL)
+    else:
+        son()
+
+#numProcess = getProcess(getArg())
+#forking(numProcess)
+
+
+def main():
+    forking(1)
+
+
+if __name__ == "__main__":
+    main()
