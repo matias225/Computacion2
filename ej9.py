788fc688c40e3ab47d9914b7f8d9844bf4dc1082
Matias <mati.kbza22@gmail.com>
2020-05-04 18:52:03 -0300

    Ej9 terminado

--- ej9.py
+++ ej9.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 from time import sleep
 from os import fork, getpid, getppid, pipe, fdopen, wait, kill, close
@@ -17,48 +17,54 @@ def main():
     signal(SIGUSR1, funcUSR1)
     signal(SIGUSR2, funcUSR2)
     r, w = pipe()
-    pAid = getpid()   
-    if fork():
-        close(w)
-        r = fdopen(r)
+    pAid = getpid()
+    pBid = fork()   
+    if pBid:
         # Proceso A
-        print("A: "+str(getpid()))
+        close(w)
+        print("Proceso A: "+str(getpid()))
+        pause()
+        # Enviar señal USR1 al Proceso B
+        kill(pBid, SIGUSR1)
         # Cuando reciba la USR2, leer el contenido del pipe y mostrar por pantalla
         pause()
-        sleep(2)
+        r = fdopen(r)
         print("Contenido del pipe: \n")
-        string = r.readline()
-        print(string)
+        for line in r:
+            print(line)
         r.close()
         exit()
     else:
-        close(r)
-        w = fdopen(w, 'w')
         # Proceso B
-        pBid = str(getpid())
-        print("B: "+pBid+", mi padre es: "+str(getppid()))
-        
-        # Escribir en pipe "Mensaje 1 (PID=BBBB)\n"
-        msg = "Mensaje 1 (PID="+pBid+")\n"
-        print(msg)
-        w.write(msg)
-        w.close()
-        # Enviar USR1 al Proceso C
-        #kill()
-        
-        if not fork():
-            #w = fdopen(w, 'w')
+        close(r)
+        sleep(1)
+        pCid = fork()    
+        if not pCid:
             # Proceso C
-            pCid = str(getpid())
-            print("C: "+str(getpid())+", mi padre es: "+str(getppid()))
+            pause()
+            w = fdopen(w, 'w')
+            print("Proceso C: "+str(getpid())+", mi padre es: "+str(getppid())+"\n")
             # Escribir en el pipe el mensaje "Mensaje 2 (PID=CCCC)\n".
-            #msg = "Mensaje 2 (PID="+pCid+")\n"
-            #print(msg)
-            #w.write(msg)
+            msg = "Mensaje 2 (PID = "+str(getpid())+")\n"
+            w.write(msg)
+            w.close()
             # Enviar al Proceso A, la USR2
-            print(pAid)
+            sleep(1)
             kill(pAid, SIGUSR2)
-            #w.close()
+        else:
+            # Proceso B
+            w = fdopen(w, 'w')
+            pBid = str(getpid())
+            print("Proceso B: "+pBid+", mi padre es: "+str(getppid()))
+            # Escribir en pipe "Mensaje 1 (PID=BBBB)\n"
+            msg = "Mensaje 1 (PID = "+pBid+")\n"
+            w.write(msg)
+            # Enviar USR2 al Proceso A
+            kill(pAid, SIGUSR2)
+            sleep(1)    
+            # Enviar USR1 al Proceso C
+            kill(pCid, SIGUSR1)
+
 
 if __name__ == "__main__":
     main()
