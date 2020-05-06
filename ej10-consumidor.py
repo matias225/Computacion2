7447f94e4774d28d83cc19dccf84586d112c3fb3
Matias <mati.kbza22@gmail.com>
2020-05-05 22:54:15 -0300

    Ejercicio 10 terminado

--- ej10-consumidor.py
+++ ej10-consumidor.py
@@ -3,13 +3,16 @@
 from os import fork, pipe, close, fdopen
 from time import sleep
 
+
 def main():
+    path = "/tmp/fifo"
+    p = open(path, 'r')
     r, w = pipe()
     if fork():
         # Padre
         close(r)
         w = fdopen(w, 'w')
-        w.write('hola')
+        w.write(p.readline())
         w.close()
         sleep(1)
     else:
--- ej10-productor.py
+++ ej10-productor.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 
-def main():
-    pass
+from os import mkfifo, path
+from sys import argv
 
+def getString():
+    string = argv[1:]
+    msg = ' '.join(string)
+    return msg
+
+
+def main():
+    msg = getString()
+    fPath = '/tmp/fifo'
+    if not path.exists(fPath):
+        mkfifo(fPath)
+    f = open(fPath, 'w')
+    f.write(msg)
+    f.close()
+    
 
 if __name__ == "__main__":
     main()
