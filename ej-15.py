627bf3d8a9bee75fb6c9e0a115c3523bd5110b76
Matias <mati.kbza22@gmail.com>
2020-06-10 13:31:51 -0300

    Ej 15 sin terminar

--- ej-15.py
+++ ej-15.py
@@ -2,11 +2,26 @@
 
 import multiprocessing as mp
 
-def child():
-    pass
+
+def child(r,w,n):
+    print("Hijo ", n)
+    if n == 1:
+        r.close() 
+        msg = input()
+        print("Hijo envia: "+msg)
+        w.send(msg)
+    if n == 2:
+        w.close()
+        print("Hijo recibe: "+r.recv())
+    r.close()
+    w.close()
+
 
 if __name__ == "__main__":
-    p = mp.Process(target=child)
-    p.start()
-    p.join()
-
+    r, w = mp.Pipe()
+    p1 = mp.Process(target=child, args=(r,w,1))
+    p2 = mp.Process(target=child, args=(r,w,2))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
