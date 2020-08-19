b8f0cc649935a02c31a1e2448aeee87e3032e710
Matias <mati.kbza22@gmail.com>
2020-08-19 13:29:08 -0300

    Ej 18 modificado

--- ej18-client.py
+++ ej18-client.py
@@ -3,6 +3,7 @@
 import socket
 import getopt
 import sys
+import os
 
 def getOptions():
     try:
@@ -22,5 +23,24 @@ for (opts, arg) in options:
         protocol = arg
 
 
+def createSocket(host, port):
+    host = host
+    port = port
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print("Failed to create socket")
+        exit()
+    s.connect((host, port))
+   # time = s.recv(1024).decode()
+    time = s.recv(1024)
+    try:
+        print("Time: ",time.decode())
+    except UnicodeDecodeError:
+        print("Error at decoding server response")
+
+
 if __name__ == "__main__":
-    print(port, host, protocol)
+    if protocol == 'tcp':
+        print('Protocolo TCP:')
+        createSocket(host, port)
