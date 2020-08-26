d82f8c30a568621795d4772b629bad6ac0e479cf
Matias <mati.kbza22@gmail.com>
2020-08-26 15:01:52 -0300

    Ej20 avanzado

--- ej20-server.py
+++ ej20-server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-from multiprocessing import Process as proc, Lock as l
+from multiprocessing import Process as proc, Lock
 from getopt import getopt, GetoptError
-from sys import argv
+from sys import argv, path
 from socket import socket, AF_INET, SOCK_STREAM
 
 
-def client(clientSocket, addr, serverSocket):
+def client(clientSocket, addr, serverSocket, l):
     while True:
         data = clientSocket.recv(1024)
         print("Address: %s " % str(addr))
@@ -15,22 +15,45 @@ def client(clientSocket, addr, serverSocket):
         if msg == 'ABRIR':
             resp = 'Ingrese un nombre de archivo...'
             clientSocket.send(resp.encode('ascii'))
-            clientresp = clientSocket.recv(1024)
-            filename = clientresp.decode('ascii')
-            fd = open(filename, 'r')
-            text = fd.readlines()
-            print(filename)
-            for i in text:
-                print(i)
-            #msg = 'Que desea hacer con el archivo?'
-            #clientSocket.send(msg.encode('ascii'))
-        if msg == 'CERRAR':
-            print('Recibido cerrar')
-        if msg == 'AGREGAR':
-            print('Recibido agregar')
-        if msg == 'LEER':
-            print('Recibido leer')
-        if data.decode('ascii') == 'exit':
+            clientResp = clientSocket.recv(1024)
+            filename = clientResp.decode('ascii')
+            fd = open(filename, 'a')
+            fd.flush()
+            fileisopen = 1
+            msg = 'Desea hacer algo mas con el archivo?'
+            clientSocket.send(msg.encode('ascii'))
+        if fileisopen == 1:
+            if msg == 'CERRAR':
+                l.acquire()
+                fd.close()
+                l.release()
+                msg = 'Archivo cerrado'
+                clientSocket.send(msg.encode('ascii'))
+            if msg == 'AGREGAR':
+                print('Recibido agregar')
+                resp = 'Ingrese el texto que desea agregar:'
+                clientSocket.send(resp.encode('ascii'))
+                clientresp = clientSocket.recv(1024)
+                l.acquire()
+                fd = open(filename, 'a')
+                fd.write('\n'+clientresp.decode('ascii'))
+                fd.flush()
+                l.release()
+                msg = 'Desea hacer algo mas con el archivo?'
+                clientSocket.send(msg.encode('ascii'))
+            '''
+            if msg == 'LEER':
+                fd = open(filename, 'r')
+                # Lista con las lineas leidas
+                lines = fd.readlines()    
+                fd.close()
+                # Junto todas las lineas en un solo mensaje
+                readedlines = ''.join(lines)
+                clientSocket.send(readedlines.encode('ascii'))
+            '''
+        else:
+            clientSocket.send('Debe abrir un archivo primero...'.encode('ascii'))
+        if msg == 'exit':
             serverSocket.close()
             break
 
@@ -56,16 +79,16 @@ for (opts, arg) in options:
 def createSocketTCP(port):
     port = port
     host = ""
+    l = Lock()
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind((host, port))
     serverSocket.listen(5)
     print('Server waiting for clients...')
     while True:
         clientSocket, addr = serverSocket.accept()
-        p = proc(target=client,args=(clientSocket,addr,serverSocket))
+        p = proc(target=client,args=(clientSocket,addr,serverSocket,l))
         p.start()
 
 
 if __name__ == "__main__":
     createSocketTCP(port)
-    print('ABRIR\nCERRAR\nAGREGAR\nLEER')
