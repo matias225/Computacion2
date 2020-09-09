a7e1014524fb0d76c3745cd0e9afbc3989dfa5a0
Matias <mati.kbza22@gmail.com>
2020-09-09 13:38:04 -0300

    Ejercicio 24 terminado

--- ej24-client-hash.py
+++ ej24-client-hash.py
@@ -7,7 +7,7 @@ from socket import socket, AF_INET, SOCK_STREAM
 
 def getOptions():
     if len(argv[1:]) == 0:
-        print('Por favor ingrese: -p seguido del puerto, -h seguido del host, -c seguido del archivo, --hash seguido del hash')
+        print('Por favor ingrese: -p seguido del puerto, -h seguido del host, -c seguido de una cadena entre '', --hash seguido del hash')
         exit()
     try:
         (opts, arg) = getopt(argv[1:], 'p:h:c:', ['hash='])
@@ -23,23 +23,29 @@ for (opts, arg) in options:
     if opts == '-h':
         host = arg
     if opts == '-c':
-        filename = arg
+        string = arg
     if opts == '--hash':
         hash_function = arg
 
 
-def createSocketTCP(port, host, hash_function, filename):
-    port = port
-    host = host
+def createSocketTCP(port, host, hash_function, string):
     try:
         clientSocket = socket(AF_INET, SOCK_STREAM)
     except error:
         print("Failed to create socket")
         exit()
     clientSocket.connect((host, port))
+    sendMsg(clientSocket, hash_function, string)
+
+
+def sendMsg(clientSocket, hash_function, string):
+    msg = hash_function+'.'+string
+    clientSocket.send(msg.encode('ascii'))
+    data = clientSocket.recv(1024)
+    msg = data.decode('ascii')
+    print('El hash '+hash_function+' de la cadena "'+string+'" es: \n'+msg)
 
 
 if __name__ == "__main__":
-    #print(host, port, filename, hash_function)
-    createSocketTCP(port, host, hash_function, filename)
+    createSocketTCP(port, host, hash_function, string)
     
--- ej24-server-hash.py
+++ ej24-server-hash.py
@@ -5,6 +5,7 @@ from getopt import getopt, GetoptError
 from socket import socket, AF_INET, SOCK_STREAM
 from multiprocessing import Process
 from threading import Thread
+from hashlib import sha1, sha224, sha256, sha384, sha512, sha3_224, sha3_256, sha3_384, sha3_512
 
 
 threading = False
@@ -33,40 +34,65 @@ for (opts, arg) in options:
 
 
 def serverMultiprocessing(port):
-    port = port
     host = ''
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind((host, port))
     serverSocket.listen(5)
-    print("Server escuchando...")
+    print("Server iniciado con multiprocessing, escuchando...")
     while True:
         clientSocket, addr = serverSocket.accept()
-        p = Process(target=hash,args=(clientSocket,addr))
+        p = Process(target=receiveMsg,args=(clientSocket,addr))
         p.start()
 
 
 def serverThreading(port):
-    port = port
     host = ''
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind((host, port))
     serverSocket.listen(5)
-    print("Server escuchando...")
+    print("Server iniciado con threading, escuchando...")
     while True:
         clientSocket, addr = serverSocket.accept()
-        th = Thread(target=hash,args=(clientSocket,addr))
+        th = Thread(target=receiveMsg,args=(clientSocket,addr))
         th.start()
 
 
-def hash():    
-    #calcular hash en python
+def receiveMsg(clientSocket, addr):
+    data = clientSocket.recv(1024)
+    print("Connection from address: %s " % str(addr))
+    msg = data.decode('ascii')
+    hash_function, string = msg.split('.',2)
+    resp = hash(hash_function, string)
+    clientSocket.send(resp.encode('ascii'))
+    clientSocket.close()
 
+
+def hash(hash_function, string):
+    if hash_function == 'sha1':     
+        return sha1(string.encode()).hexdigest()
+    if hash_function == 'sha224':
+        return sha224(string.encode()).hexdigest()
+    if hash_function == 'sha256':
+        return sha256(string.encode()).hexdigest()
+    if hash_function == 'sha384':
+        return sha384(string.encode()).hexdigest()
+    if hash_function == 'sha512':
+        return sha512(string.encode()).hexdigest()
+    if hash_function == 'sha3_224':
+        return sha3_224(string.encode()).hexdigest()
+    if hash_function == 'sha3_256':
+        return sha3_256(string.encode()).hexdigest()
+    if hash_function == 'sha3_384':
+        return sha3_384(string.encode()).hexdigest()
+    if hash_function == 'sha3_512':
+        return sha3_512(string.encode()).hexdigest()
+    else:
+        return 'Wrong hash function: please use sha1, sha224, sha384, sha512, sha3_224, sha3_256, sha3_384, sha3_512'
+        
 
 if __name__ == "__main__":
-    #createSocketTCP(port)
-    print(port, threading, multiprocessing)
     if multiprocessing:
-        serverMultiprocessing()
+        serverMultiprocessing(port)
     if threading:
-        serverThreading()
+        serverThreading(port)
     
